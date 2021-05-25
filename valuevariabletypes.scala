3d7445a37395a5ec284439643378fceba5baa58b
praveenpro587 <42086848+praveenpro587@users.noreply.github.com>
2021-05-25 16:04:07 +0530

    valuevariabletypes

---
+++ valuevariabletypes.scala
@@ -0,0 +1,18 @@
+object variable extends App{
+  //if we defined a variable with val it is immutable
+  val x:Int=10 
+  println(x)
+  
+  val a:String="Praveen";
+  println(a)
+  
+  val aBoolean:Boolean=false
+  val aChar:Char='b'
+  
+  println(aBoolean,aChar)
+  //Variables can be reassign
+  var y=12
+  println(y)
+  y=13
+  println(y)
+}
