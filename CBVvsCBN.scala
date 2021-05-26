21a867f9f1d86a3fc77767cb2ccb3ec3b8b14abf
praveenpro587 <42086848+praveenpro587@users.noreply.github.com>
2021-05-26 12:19:31 +0530

    Create CBVvsCBN.scala

---
+++ CBVvsCBN.scala
@@ -0,0 +1,16 @@
+object CBVvsCBN extends App{
+  
+  def callByValue(x:Long):Unit={
+    println("Call by value"+x)
+    println("Call by value"+x)
+  }
+  
+  def callByName(x: => Long):Unit={
+    println("Call by name"+x)
+    println("Call by name"+x)
+  }
+  
+  callByValue(System.nanoTime())
+  callByName(System.nanoTime())
+}
+
